ac786e4ac6feba789945d3167a38b27813857331
Bob Damiano <bobd@rockscience.net>
2024-09-04 10:55:16 -0400

    TSID Helpers - more static helpers for converting strings and longs

--- src/main/java/net/rockscience/util/TSIDUtil.java
+++ src/main/java/net/rockscience/util/TSIDUtil.java
@@ -18,7 +18,23 @@ public class TSIDUtil {
 		 return TSID_FACTORY.generate();
 	}
 
-	public static TSID.Factory getTsidFactory(int nodeCount) {
+	public static Long toLong(String stringId) {
+		return StringUtils.isEmpty(stringId) ? null : TSID.from(stringId).toLong();
+	}
+
+	public static String idToString(Long id) {
+		return id == null ? null : TSID.from(id).toString();
+	}
+
+	public static long toLongOrZero(TSID id) {
+		return id == null ? 0 : id.toLong();
+	}
+
+	public static String toStringOrNull(TSID id) {
+		return id == null ? null : id.toString();
+	}
+
+	private static TSID.Factory getTsidFactory(int nodeCount) {
 		 int nodeBits = (int) (Math.log(nodeCount) / Math.log(2));
 
 		 return TSID.Factory.builder()
@@ -29,7 +45,7 @@ public class TSIDUtil {
 			  .build();
 	}
 
-	public static TSID.Factory getTsidFactory(int nodeCount, int nodeId) {
+	private static TSID.Factory getTsidFactory(int nodeCount, int nodeId) {
 		 int nodeBits = (int) (Math.log(nodeCount) / Math.log(2));
 
 		 return TSID.Factory.builder()
@@ -39,11 +55,5 @@ public class TSIDUtil {
 			  .build();
 	}
 
-	public static Long toLong(String stringId) {
-		return StringUtils.isEmpty(stringId) ? null : TSID.from(stringId).toLong();
-	}
 
-	public static String idToString(Long id) {
-		return id == null ? null : TSID.from(id).toString();
-	}
 }
